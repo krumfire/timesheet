08da621aa696f3cd0c59bca8af1354549cdca92c
krumfire <bjohnson@krumfire.com>
2026-08-31 21:29:37 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5,12 +5,10 @@ const HOUR_LABELS = {
   holiday: 'Holiday', other: 'Other'
 };
 
-const DAY_NAMES = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
 let sigPad;
 
 function formatDate(d) {
-  return `${DAY_NAMES[d.getDay()]} ${d.getMonth() + 1}/${d.getDate()}/${String(d.getFullYear()).slice(-2)}`;
+  return `${d.getMonth() + 1}/${d.getDate()}`;
 }
 function isoDate(d) {
   return d.toISOString().slice(0, 10);
